4de8619e5c1f923f3bb92400bdc1ba4ec5ced38e
DucNguyen <nguyenngocduc94751@gmail.com>
2023-05-09 19:42:39 +0700

    first commit

---
+++ src/pages/products/index.tsx
@@ -0,0 +1,12 @@
+import Button from "@/components/Button";
+import React from "react";
+
+type Props = {};
+
+export default function index({}: Props) {
+  return (
+    <div>
+      <Button />
+    </div>
+  );
+}
